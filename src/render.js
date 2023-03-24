4b978b8c92657509b4c205f2a6c715e29476fabf
VitaliiMaC9m6uk <mac9m6uk@gmail.com>
2023-03-24 07:43:28 +0100

    Update render.js

--- src/render.js
+++ src/render.js
@@ -1,11 +1,11 @@
 export const renderCountri = (countri) => {       
-    //  const {capital,flags,name,population } = countri[0];
+     const {capital,flags,name,population } = countri[0];
      return `
-    <img src="${countri[0].flags.svg}" alt="${countri[0].name.official}flag" width="20px">
-    <h2>${countri[0].name.official}</h2>
+    <img src="${flags.svg}" alt="${name.official}flag" width="20px">
+    <h2>${name.official}</h2>
     <ul>
-        <li>capital:${countri[0].capital}</li>
-        <li>population:${countri[0].population}</li>
+        <li>capital:${capital}</li>
+        <li>population:${population}</li>
         <li>languages:${Object.values(countri[0].languages)}</li>
     </ul>
     `
